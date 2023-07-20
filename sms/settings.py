f5c070e67d58b882b59948a9cb4d0e62f6e5285e
silas003 <silaskumi4@gmail.com>
2023-07-20 11:51:39 +0000

    school admin

--- sms/settings.py
+++ sms/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-742il3+)cm3ztyh%kvfn(z#4gbmxk!w!-bd1@-9$7j603p226p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
